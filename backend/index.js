92e58338a96ddebcff40c41bafe8483b0d17221f
Aditya Thakur <aditya@example.com>
2026-02-26 11:00:00 +0530

    add main index.js for backend

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import connectDb from './config/db.js'
+import userRouter from './routes/user.routes.js'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import authRouter from './routes/auth.routes.js'
+import geminiResponse from './gemini.js'
+
+
+
+const app = express()
+
+app.use(cors({
+    origin: ["http://localhost:5173", "https://ai-virtual-assistant-8yqf.onrender.com"],
+    credentials: true,
+}))
+const port = process.env.PORT || 5000 
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/auth", userRouter)
+app.use("/api/user", authRouter)
+
+
+
+app.listen(port,()=>
+{
+    connectDb();
+    console.log(`Server is started`);
+    
+})
